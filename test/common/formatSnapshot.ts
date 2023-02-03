891d8b7d59767bfc0eb08192d10b68190b384e16
hmatsu47 <hmatsu47@gmail.com>
2023-02-04 00:07:30 +0900

    パッケージアップデート

--- test/common/formatSnapshot.ts
+++ test/common/formatSnapshot.ts
@@ -1,4 +1,4 @@
 // スナップショットを整形する（インラインスタイル名と改行）
 export const formatSnapshot = (snapshot: string) => {
-  return snapshot.replace(/css-\w{6}/g, "css-xxxxxx");
+  return snapshot.replace(/css-\w{8}/g, "css-xxxxxxxx");
 };
